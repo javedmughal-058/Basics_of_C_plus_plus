a578ea8f58cb8b7781336c3c5eeed619a5eb3987
Muhammad_Javed <91019922+javedmughal-058@users.noreply.github.com>
2021-10-15 23:52:36 +0500

    If else in C++

---
+++ if else in c++.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"Enter a: ";
+	cin>>a;
+	cout<<"Enter b: ";
+	cin>>b;
+	if(a>b)
+	printf("A is geater than B");
+	else 
+	printf("B is greater than A");
+}
